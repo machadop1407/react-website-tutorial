afe8d201e2b7602765b0e00897a506adedccb570
soumya1014 <bairagisoumyadip@gmail.com>
2023-09-16 20:46:48 +0530

    ch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Me() {
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
         <h1> Pedro's Pizzeria </h1>
-        <p> PIZZA TO FIT ANY TASTE</p>
+        <p> PIZZA TO FIT ANY </p>
         <Link to="/menu">
           <button> ORDER NOW </button>
         </Link>
